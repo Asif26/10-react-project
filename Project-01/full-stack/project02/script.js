b6b3a546866cc55d60731665f134720402c28a20
AsifBashir <56689372+Asif26@users.noreply.github.com>
2023-07-08 23:54:04 +0500

    learning Javascript

--- Project-01/full-stack/project02/script.js
+++ Project-01/full-stack/project02/script.js
@@ -1,37 +1,32 @@
-// Get The Dom Elements
+// Get DOM Elements
 
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.row.seat:not(.occupied');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-const movieSelect =  document.getElementById('movie')
+const movieSelect = document.getElementById('movie');
 
 
-let ticketPrice = movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 
-
-function updateSelectedCount (){
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
     const selectedSeatsCount = selectedSeats.length;
-    console.log(selectedSeatsCount)
+    console.log(selectedSeatsCount);
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
+container.addEventListener('click', e => {
+    if(e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')){
+        e.target.classList.toggle("selected")
+        updateSelectedCount()
+    }
+});
 
-// Event Listneres 
-//1. Event  listner for conatiner
-container.addEventListener('click',  e => {
- if (e.target.classList.contains('seat') && 
- !e.target.classList.contains('occupied')){
-     e.target.classList.toggle('selected')
-     updateSelectedCount();
-}
-})
 
-//2. Event  listner for movie select
-
-movieSelect.addEventListener('change', e => {
-    ticketPrice = e.target.value;
+movieSelect.addEventListener('change', e =>{
+    ticketPrice = +e.target.value;
     updateSelectedCount()
-})
+} )
